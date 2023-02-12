cc66c9c9329e845c7ef3894bb7dc9dd0731f359a
eshitaarora <78889909+eshitaarora@users.noreply.github.com>
2023-02-13 00:26:20 +0530

    Update 102003522.py

--- 102003522.py
+++ 102003522.py
@@ -1,16 +1,15 @@
+import streamlit as st
 import os
 os.system("pip install moviepy")
 os.system("pip install youtube_search")
 os.system("pip install pytube")
 os.system("pip install pydub")
 os.system("pip install requests")
-import streamlit as st
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
-# import streamlit as st
 from time import sleep
 import re
 import zipfile
